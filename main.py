7a469dfacd70610c6283037eb71b6f4d6ecf6acb
frugiam <frugiam@oregonstate.edu>
2024-01-22 17:49:10 -0800

    average.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Author: Michelle Frugia
+# GitHub username: frugiam
+# Date: 01/18/2024
+# Description: Project 2a
+print("Please enter five numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+print("The sum is", num_1 + num_2 + num_3 + num_4 + num_5)
+average = sum/5
+print("The average of those numbers is: ")
